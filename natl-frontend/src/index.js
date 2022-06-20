9242838a633f152c30093b3c916fc0e345079330
Arthur Gartner <Arthur.Gartner@gmail.com>
2022-06-20 19:52:29 -0400

    Change

--- natl-frontend/src/index.js
+++ natl-frontend/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
 
-// Main
+//Main
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
